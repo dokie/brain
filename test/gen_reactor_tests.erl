9e76146123f9d15653627d10fcb2c769ceeff964
Dr Mike Williams <mike@qixsoft.co.uk>
2014-02-21 21:31:52 +0000

    Partial re-design of Reactor

--- test/gen_reactor_tests.erl
+++ test/gen_reactor_tests.erl
@@ -17,14 +17,11 @@ reactor_start_test() ->
   meck:new(dummy_reactor),
   meck:expect(dummy_reactor, init, fun(_Opts) -> ok end),
   meck:expect(dummy_reactor, reactants, fun() -> [{input, float}] end),
-  meck:expect(dummy_reactor, reaction, fun([{input, X}]) -> [{input_reaction, X * 2.0}] end),
 
-  meck:new(tuple_space_server),
-  meck:expect(tuple_space_server, out, fun(_) -> ok end),
+  meck:new(tuple_space_server),  meck:expect(tuple_space_server, out, fun(_) -> ok end),
   meck:expect(tuple_space_server, in,  fun({input, float}) -> {input, 1.23} end),
 
   {ok, _Pid} = ?REACT:start(dummy, dummy_reactor, []),
-  ?assert(meck:called(tuple_space_server, out, [{input_reaction, 2.46}])),
   ?assert(meck:validate(tuple_space_server)),
   ?assert(meck:validate(dummy_reactor)),
 
