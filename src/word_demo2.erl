6668481df00f42c49b40662d90f68d40bf36e54d
dokie <dokieboy@gmail.com>
2014-05-16 17:39:44 +0100

    Demo 4 Improved

--- src/word_demo2.erl
+++ src/word_demo2.erl
@@ -10,11 +10,11 @@
 -author("dokie").
 
 %% API
--export([master_init/2, master_run/2, worker/2, worker_run/2, gather/0, gatherer_run/1]).
+-export([master_init/3, master_run/2, worker/2, worker_run/2, gather/1, gatherer_run/2]).
 
 -record(counts, {dict}).
 
-master_init(Subsize, Filename) ->
+master_init(Subsize, Filename, NumWorker) ->
   brain:start(),
   %% Setup Master
   {ok, {Words, M}} = word_generator:init([{filename, Filename}, {subsize, Subsize}]),
@@ -26,7 +26,7 @@ master_init(Subsize, Filename) ->
   Letters = [list_to_atom([L]) || L <- lists:seq(97,122)],
   InitialLetterDict = lists:foldl(LetterFold, dict:new(), Letters),
   Counts = #counts{dict = InitialLetterDict},
-  ok = tuple_space_server:out({counts, Counts}),
+  lists:foreach(fun (E) -> ok = tuple_space_server:out({counts, Counts, E}) end, lists:seq(1, NumWorker)),
   Total = {total, 0},
   %% Seed the Brain
   ok = tuple_space_server:out(Total),
@@ -51,30 +51,43 @@ worker(M, Identity) ->
     run ->
         {words, List, Index} = tuple_space_server:in({words, [{string, M}], Identity - 1 }),
         io:format("[~pS:~pI]", [Identity, Index + 1]),
-        {_, #counts{dict = CurrentCounts}} = tuple_space_server:in({counts, {record, counts}}),
+        {counts, #counts{dict = CurrentCounts}, Identity} = tuple_space_server:in({counts, {record, counts}, Identity}),
         UpdatedCounts = lists:foldl(LetterFold, CurrentCounts, List),
         Counts = #counts{dict = UpdatedCounts},
-        ok = tuple_space_server:out({counts, Counts}),
+        ok = tuple_space_server:out({counts, Counts, Identity}),
         {total, CurrentTotal} = tuple_space_server:in({total, int}),
         ok = tuple_space_server:out({total, CurrentTotal + M}),
         io:format("[~pE:~pI]", [Identity, Index + 1]),
         Worker = spawn(word_demo2, worker, [M, Identity]),
         Worker ! run;
-        stop -> ok
+     stop -> ok
   end.
 
 worker_run(M, NumWorker) ->
   Workers = [spawn(word_demo2, worker, [M, X]) || X <- lists:seq(1, NumWorker)],
   lists:foreach(fun(W) -> W ! run end, Workers).
 
-gather() ->
+gather(NumWorker) ->
   receive
     {run, From, N} ->
       tuple_space_server:in({total, fun(I) -> I >= N end}),
-      {counts, #counts{dict = CountDict}} = tuple_space_server:in({counts, {record, counts}}),
-      ResetCountDict = dict:map(fun(_K,_V) -> 0 end, CountDict),
-      tuple_space_server:out({counts, #counts{dict = ResetCountDict}}),
       tuple_space_server:out({total, 0}),
+
+      LetterFold = fun (Letter, Dict) ->
+        _NewDict = dict:update(Letter, fun (Old) -> Old + 1 end, 0, Dict)
+      end,
+
+      Letters = [list_to_atom([L]) || L <- lists:seq(97,122)],
+      InitialLetterDict = lists:foldl(LetterFold, dict:new(), Letters),
+
+      CountDict = lists:foldl(fun (E, Acc) ->
+        {counts, #counts{dict = WorkerCounts}, _I} = tuple_space_server:in({counts, {record, counts}, E}),
+        dict:merge(fun (_K, V1, V2) -> V1 + V2 end, Acc, WorkerCounts) end,
+        InitialLetterDict, lists:seq(1, NumWorker)),
+
+      ResetCountDict = dict:map(fun(_K,_V) -> 0 end, CountDict),
+      lists:foreach(fun (E) -> ok = tuple_space_server:out({counts, #counts{dict = ResetCountDict}, E}) end, lists:seq(1, NumWorker)),
+
       Start = tuple_space_server:in({int, int, int}),
       End = now(),
       DiffMS = timer:now_diff(End, Start) / 1000,
@@ -82,18 +95,15 @@ gather() ->
       PrintResult = fun ({Letter, Count}) ->
         io:format("~p : ~w~n", [Letter, Count])
       end,
-      timer:sleep(1000),
       io:format("~nThe operation took: ~p milliseconds~n", [DiffMS]),
       lists:foreach(PrintResult, Results),
       From ! done,
-      G = spawn(word_demo2, gather, []),
+      G = spawn(word_demo2, gather, [NumWorker]),
       G ! {run, From, N};
     stop ->
       ok
   end.
 
-gatherer_run(N) ->
-  Gatherer = spawn(word_demo2, gather, []),
+gatherer_run(N, NumWorker) ->
+  Gatherer = spawn(word_demo2, gather, [NumWorker]),
   Gatherer ! {run, self(), N}.
-
-
